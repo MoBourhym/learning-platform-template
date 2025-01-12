a9d3d549c827061a9f7e743faaf7e1f7791201db
BOURHYM <mohammedbourhym9@gmail.com>
2025-01-12 02:52:51 +0100

    Redis Service

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,39 @@
-// Question : Comment gérer efficacement le cache avec Redis ?
-// Réponse :
-// Question: Quelles sont les bonnes pratiques pour les clés Redis ?
-// Réponse :
+const db = require('../config/db');
 
-// Fonctions utilitaires pour Redis
-async function cacheData(key, data, ttl) {
-    // TODO: Implémenter une fonction générique de cache
+async function set(key, data, ttl = 3600) {
+  try {
+    const redis = db.getRedis();
+    await redis.set(key, data, 'EX', ttl);
+  } catch (error) {
+    console.error('Redis set error:', error);
+    // Don't throw - treat cache errors as non-fatal
   }
-  
-  module.exports = {
-    // TODO: Exporter les fonctions utilitaires
-  };
+}
+
+async function get(key) {
+  try {
+    const redis = db.getRedis();
+    return await redis.get(key);
+  } catch (error) {
+    console.error('Redis get error:', error);
+    return null;
+  }
+}
+
+async function deleteByPattern(pattern) {
+  try {
+    const redis = db.getRedis();
+    const keys = await redis.keys(pattern);
+    if (keys.length > 0) {
+      await redis.del(keys);
+    }
+  } catch (error) {
+    console.error('Redis delete pattern error:', error);
+  }
+}
+
+module.exports = {
+  set,
+  get,
+  deleteByPattern
+};
